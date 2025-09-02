0d762bd6552abe41e5a6033d4884f8378e85639c
dmitriy-zverev <dimazverev716@gmail.com>
2025-09-02 14:07:21 +0300

    update upload thumbnail handler to store images in base64 format in database

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	maxMemory := 10 << 20
+	err = r.ParseMultipartForm(int64(maxMemory))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse multipart form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, fileHandler, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get thumbnail from form", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := fileHandler.Header.Get("Content-Type")
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse image data from file", err)
+		return
+	}
+
+	videoRow, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get video from database", err)
+		return
+	}
+
+	if videoRow.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized access", err)
+		return
+	}
+
+	base64ImageData := base64.StdEncoding.EncodeToString(imageData)
+	dataUrl := fmt.Sprintf(
+		"data:%s;base64,%s",
+		mediaType,
+		base64ImageData,
+	)
+
+	videoRow.ThumbnailURL = &dataUrl
+
+	if err := cfg.db.UpdateVideo(videoRow); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't update video in database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoRow)
 }
