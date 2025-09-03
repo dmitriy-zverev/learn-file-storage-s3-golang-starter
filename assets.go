dbef1c8856c810617fb8eb1275a0635c9a270e4d
dmitriy-zverev <dimazverev716@gmail.com>
2025-09-03 17:48:32 +0300

    add video uploading handler with storing logic to aws s3

--- assets.go
+++ assets.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"math"
 	"os"
+	"os/exec"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -9,4 +13,47 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	cmd := exec.Command(
+		"ffprobe",
+		"-v", "error",
+		"-print_format", "json",
+		"-show_streams",
+		filePath,
+	)
+
+	reader := bytes.Buffer{}
+
+	cmd.Stdout = &reader
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	type stream struct {
+		Width  float64 `json:"width"`
+		Height float64 `json:"height"`
+	}
+
+	type response struct {
+		Streams []stream `json:"streams"`
+	}
+
+	resp := response{}
+	if err := json.NewDecoder(&reader).Decode(&resp); err != nil {
+		return "", err
+	}
+
+	remainder := resp.Streams[0].Width / resp.Streams[0].Height
+
+	if math.Abs(remainder-1.77777) < 0.001 {
+		return "16:9", nil
+	}
+
+	if math.Abs(remainder-0.5625) < 0.001 {
+		return "9:16", nil
+	}
+
+	return "other", nil
 }
--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,137 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<30)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	videoRow, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get video from database", err)
+		return
+	}
+
+	if videoRow.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized access", err)
+		return
+	}
+
+	file, fileHandler, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(fileHandler.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get media type", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Cannot use non mp4 file format", err)
+		return
+	}
+
+	tmpFile, err := os.CreateTemp("/tmp", "tubely-upload-*.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't create temp file", err)
+		return
+	}
+	defer tmpFile.Close()
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := io.Copy(tmpFile, file); err != nil {
+		tmpFile.Close()
+		respondWithError(w, http.StatusBadRequest, "Couldn't copy data to temp file", err)
+		return
+	}
+
+	if _, err := tmpFile.Seek(0, io.SeekStart); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't set seek to start of temp file", err)
+		return
+	}
+
+	randFileKey := make([]byte, 32)
+	if _, err := rand.Read(randFileKey); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't generate random file key", err)
+		return
+	}
+
+	aspectRation, err := getVideoAspectRatio(tmpFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get aspect ratio of temp file", err)
+		return
+	}
+
+	randFileName := base64.RawURLEncoding.EncodeToString(randFileKey) + ".mp4"
+	fullObjectPath := ""
+	switch aspectRation {
+	case "16:9":
+		fullObjectPath = "landscape/" + randFileName
+	case "9:16":
+		fullObjectPath = "portrait/" + randFileName
+	case "other":
+		fullObjectPath = "other/" + randFileName
+	}
+
+	if _, err := cfg.s3Client.PutObject(
+		context.TODO(),
+		&s3.PutObjectInput{
+			Bucket:      &cfg.s3Bucket,
+			Key:         &fullObjectPath,
+			Body:        tmpFile,
+			ContentType: &mediaType,
+		},
+	); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't load video file to aws", err)
+		return
+	}
+
+	videoUrlFilePath := fmt.Sprintf(
+		"https://storage.yandexcloud.net/%s/%s",
+		cfg.s3Bucket,
+		fullObjectPath,
+	)
+	videoRow.VideoURL = &videoUrlFilePath
+
+	if err := cfg.db.UpdateVideo(videoRow); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't update video in database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoRow)
+}
